51ec4925fb3b0cb3685965cb2531f25ed0142225
Adedoyin <59482569+DevAdedoyin@users.noreply.github.com>
2023-03-03 22:46:29 +0000

    Color module added

---
+++ color_module.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  3 18:53:39 2023
+
+@author: Adedoyin Idris
+"""
+
+# A function that returns list of some colors
+def colors():
+    list_of_colors = [
+        "#556B2F", "#FF8C00", "#9932CC", "#8B0000", "#E9967A", 
+        "#8FBC8F", "#483D8B", "#2F4F4F", "#00CED1", "#9400D3",
+        "#FF1493", "#00BFFF", "#D2691E", "#FF7F50", "#6495ED", 
+        "#FF6347", "#DC143C", "#00FFFF", "#00008B", "#008B8B",
+        "#B8860B", "#A9A9A9", "#006400", "#BDB76B", "#6A5ACD", 
+        "#556B2F", "#4682B4", "#FF8C00", "#9932CC", "#8B0000",
+        "#E9967A", "#00FF7F", "#483D8B"
+        ]
+    
+    return list_of_colors
+    
